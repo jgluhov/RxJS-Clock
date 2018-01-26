ead3b3c650ad93a23b6c2dee1d3c13c2cebfe074
Evgeny Glukhov <evglukhov@luxoft.com>
2018-01-26 10:33:53 +0300

    add clock animation

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,19 @@
 const canvas: HTMLCanvasElement = document.querySelector('#screen');
 const context: CanvasRenderingContext2D = canvas.getContext('2d');
 
+const FONT_HEIGHT: number = 16;
+const FONT: string = `${FONT_HEIGHT}px Consolas`;
+
 const MARGIN: number = 50;
+const NUMERAL_SPACING: number = 20;
 const RADIUS: number = canvas.width / 2 - MARGIN;
+const HAND_RADIUS: number = RADIUS + NUMERAL_SPACING;
+const HAND_TRUNCATION: number = canvas.width / 25;
+const HOUR_HAND_TRUNCATION: number = canvas.width / 10;
+
+function clear(): void {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 function drawCircle(): void {
     context.beginPath();
@@ -13,8 +24,61 @@ function drawCircle(): void {
     context.stroke();
 }
 
-function drawClock(): void {
-    drawCircle();
+function drawCenter(): void {
+    context.beginPath();
+    context.arc(canvas.width / 2, canvas.height / 2, 5, 0, Math.PI * 2, true);
+    context.fill();
 }
 
+function drawNumerals(): void {
+    const numerals: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    numerals.forEach((numeral: number) => {
+        const angle: number = Math.PI / 6 * (numeral - 3);
+        const numeralWidth: number = context.measureText(numeral.toString()).width;
+
+        context.fillText(
+            numeral.toString(),
+            canvas.width / 2 + Math.cos(angle) * HAND_RADIUS - numeralWidth / 2,
+            canvas.height / 2 + Math.sin(angle) * HAND_RADIUS + FONT_HEIGHT / 4
+        );
+    });
+}
+
+function drawHand(time: number, stepAngle: number, radius: number): void {
+    const angle: number = time * stepAngle - Math.PI / 2;
+
+    context.moveTo(canvas.width / 2, canvas.height / 2);
+    context.lineTo(
+        canvas.width / 2 + Math.cos(angle) * radius,
+        canvas.height / 2 + Math.sin(angle) * radius
+    );
+    context.stroke();
+}
+
+function drawHands(): void {
+    const date: Date = new Date();
+    const hour: number = date.getHours() % 12;
+
+    drawHand(hour, Math.PI * 2 / 12, RADIUS - HAND_TRUNCATION - HOUR_HAND_TRUNCATION);
+    drawHand(date.getMinutes(), Math.PI * 2 / 60, RADIUS - HAND_TRUNCATION);
+    drawHand(date.getSeconds(), Math.PI * 2 / 60, RADIUS - HAND_TRUNCATION);
+}
+
+function init(): void {
+    context.font = FONT;
+}
+
+function drawClock(): void {
+    clear();
+
+    drawCircle();
+    drawCenter();
+    drawNumerals();
+    drawHands();
+
+    requestAnimationFrame(drawClock);
+}
+
+init();
 drawClock();
