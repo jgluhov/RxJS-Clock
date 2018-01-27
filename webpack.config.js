0aac17fb0189d9f0a3ff4c720b0bec90f76ee61d
Evgeny Glukhov <evglukhov@luxoft.com>
2018-01-27 18:52:17 +0300

    add uglify and docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: "./src",
@@ -33,7 +34,8 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html'
-        })
+        }),
+        new UglifyJsPlugin()
     ],
 
     devServer: {
